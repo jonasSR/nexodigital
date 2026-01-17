127496403ae6a41346deeeb74c13c7d59236afec
Jonas SR <jonassoaresraymund@gmail.com>
2026-01-17 17:13:07 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1, // 1 card em telas pequenas
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        // Quando a tela for maior que 768px (Desktop), mostra 2
+        768: {
+            slidesPerView: 2,
+        }
+    }
+});
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    // Ativa as setas
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+        }
+    }
+});
